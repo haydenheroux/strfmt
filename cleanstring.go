56489bcc4dcd9599ac04a49d476367622a127b48
Hayden Heroux <haydenheroux@gmail.com>
2023-07-27 05:57:23 -0400

    Add exceptions for apostrophe.

--- cleanstring.go
+++ cleanstring.go
@@ -1,76 +1,81 @@
 package cleanstring
 
 import (
-  "strings"
+	"strings"
 )
 
 // isAlpha determines if the provided rune is part of the alphabet runes.
 // isAlpha checks if the rune is in either the lowercase or uppercase rune region.
 func isAlpha(r rune) bool {
-  isUppercase := 'A' <= r && r <= 'Z'
-  isLowercase := 'a' <= r && r <= 'z' 
-  return isUppercase || isLowercase
+	isUppercase := 'A' <= r && r <= 'Z'
+	isLowercase := 'a' <= r && r <= 'z'
+	return isUppercase || isLowercase
 }
 
 // isNumber determines if the provided rune is part of the numerical runes.
 func isNumber(r rune) bool {
-  return '0' <= r && r <= '9'
+	return '0' <= r && r <= '9'
 }
 
 // isSpecial determines if r is a non-alphanumeric rune exempt from being replaced by an underscore.
 func isSpecial(r rune) bool {
-	return strings.IndexRune("+", r) != -1
+	return strings.ContainsAny(string(r), "+")
+}
+
+// skip determines if r should skipped.
+func skip(r rune) bool {
+    return strings.ContainsAny(string(r), "'")
 }
 
 // toLower returns the lowercased permutation of the provided rune.
 func toLower(r rune) rune {
-  lower := strings.ToLower(string(r))[0]
-  return rune(lower)
+	lower := strings.ToLower(string(r))[0]
+	return rune(lower)
 }
 
 // Clean returns a string which is "cleanly formatted".
 // Alphanumeric characters are generally maintained.
 // Punctuation is substituted for underscores.
 func Clean(str string) string {
-  var sb strings.Builder  
+	var sb strings.Builder
 
-  var R rune
+	var R rune
 
-  for _, r := range str {
+	for _, r := range str {
 
-    if isAlpha(r) {
-      // Transform alphabet character to lowercase
-      R = toLower(r)
-    } else if isNumber(r) || isSpecial(r) {
-      // Keep original rune if it is a number
-      R = r
-    } else if R != '_' {
-      R = '_'
-    } else {
-      // Do not include multiple unbroken underscores, leaving one underscore
-      continue
-    }
+		if isAlpha(r) {
+			// Transform alphabet character to lowercase
+			R = toLower(r)
+		} else if isNumber(r) || isSpecial(r) {
+			// Keep original rune if it is a number
+			R = r
+		} else if R != '_' && !skip(r) {
+			R = '_'
+		} else {
+			// Do not include multiple unbroken underscores, leaving one underscore
+			continue
+		}
 
-    sb.WriteRune(R)
-  }
+		sb.WriteRune(R)
+	}
 
-  newStr := sb.String()
-  trimmed := strings.Trim(newStr, "_")
+	newStr := sb.String()
+	trimmed := strings.Trim(newStr, "_")
 
-  return trimmed
+	return trimmed
 }
 
 // CleanSlice returns the result of cleanly-formatting then joining a slice of strings.
 // Each string in the slice is first cleanly-formatted.
 // All strings are then joined using "+".
 func CleanSlice(strs []string) string {
-  newStrs := make([]string, len(strs))
-  
-  for i, str := range strs {
-    newStrs[i] = Clean(str)
-  }
+	newStrs := make([]string, len(strs))
 
-  return strings.Join(newStrs, "+")
+	for i, str := range strs {
+		newStrs[i] = Clean(str)
+	}
+
+	return strings.Join(newStrs, "+")
 }
 
 // CleanMap returns the result of cleanly-formatting then joining a map of strings.
@@ -78,16 +83,16 @@ func CleanSlice(strs []string) string {
 // All key-value pairs are then joined using "-".
 // All pairs are then joined using "+".
 func CleanMap(strs map[string]string) string {
-  newStrs := make([]string, len(strs))
+	newStrs := make([]string, len(strs))
 
-  i := 0
+	i := 0
 
-  for key, value := range strs {
-    cleanKey := Clean(key)
-    cleanValue := Clean(value)
-    newStrs[i] = cleanKey + "-" + cleanValue
-    i++
-  }
+	for key, value := range strs {
+		cleanKey := Clean(key)
+		cleanValue := Clean(value)
+		newStrs[i] = cleanKey + "-" + cleanValue
+		i++
+	}
 
-  return strings.Join(newStrs, "+")
+	return strings.Join(newStrs, "+")
 }
--- cleanstring_test.go
+++ cleanstring_test.go
@@ -4,84 +4,100 @@ import "testing"
 import "strings"
 
 func TestSentence(t *testing.T) {
-  got := Clean("The quick brown fox jumps over the lazy dog.")
-  want := "the_quick_brown_fox_jumps_over_the_lazy_dog"
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := Clean("The quick brown fox jumps over the lazy dog.")
+	want := "the_quick_brown_fox_jumps_over_the_lazy_dog"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestUnbrokenNonAlphanum(t *testing.T) {
-  got := Clean("Acme & Co.")
-  want := "acme_co"
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := Clean("Acme & Co.")
+	want := "acme_co"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestUnbrokenNonAlphanumHead(t *testing.T) {
-  got := Clean("... hello? Is anybody there?")
-  want := "hello_is_anybody_there"
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := Clean("... hello? Is anybody there?")
+	want := "hello_is_anybody_there"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestUnbrokenNonAlphanumLast(t *testing.T) {
-  got := Clean("What did you just say?!?!")
-  want := "what_did_you_just_say"
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := Clean("What did you just say?!?!")
+	want := "what_did_you_just_say"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestOnlyNonAlphanum(t *testing.T) {
-  got := Clean("???")
-  want := ""
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := Clean("???")
+	want := ""
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+}
+
+func TestApostrophe(t *testing.T) {
+	got := Clean("Don't say that!")
+	want := "dont_say_that"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
+}
+
+func TestQuote(t *testing.T) {
+	got := Clean("\"To be, or not to be\"")
+	want := "to_be_or_not_to_be"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestUnicode(t *testing.T) {
-  got := Clean("Hello 👋")
-  want := "hello"
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := Clean("Hello 👋")
+	want := "hello"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestOnlyUnicode(t *testing.T) {
-  got := Clean("가다")
-  want := ""
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := Clean("가다")
+	want := ""
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestSlice(t *testing.T) {
-  got := CleanSlice([]string{"One", "Two", "Three"})
-  want := "one+two+three"
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := CleanSlice([]string{"One", "Two", "Three"})
+	want := "one+two+three"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestMap(t *testing.T) {
-  got := CleanMap(map[string]string{"One": "Uno", "Two": "Dos", "Three": "Tres"})
-  want := "one-uno+two-dos+three-tres"
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	got := CleanMap(map[string]string{"One": "Uno", "Two": "Dos", "Three": "Tres"})
+	want := "one-uno+two-dos+three-tres"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
 
 func TestBoth(t *testing.T) {
-  sl := CleanSlice(strings.Split("Alpha & Beta", "&"))
-  st := Clean("Gamma")
-  m := map[string]string{sl: st}
-  got := CleanMap(m)
-  want := "alpha+beta-gamma"
-  if got != want {
-    t.Errorf("got: %s, want: %s", got, want)
-  }
+	sl := CleanSlice(strings.Split("Alpha & Beta", "&"))
+	st := Clean("Gamma")
+	m := map[string]string{sl: st}
+	got := CleanMap(m)
+	want := "alpha+beta-gamma"
+	if got != want {
+		t.Errorf("got: %s, want: %s", got, want)
+	}
 }
